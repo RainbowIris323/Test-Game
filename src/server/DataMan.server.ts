86892a3d343b50d34c70578fcece193af357ec58
RainbowIris333 <rainbowiris323@gmail.com>
2022-12-19 15:59:20 -0500

    potato again ;)

--- src/server/DataMan.server.ts
+++ src/server/DataMan.server.ts
@@ -1,15 +1,20 @@
 /* eslint-disable roblox-ts/lua-truthiness */
-import { Players, DataStoreService, ReplicatedStorage } from "@rbxts/services";
+import { Players, DataStoreService, ReplicatedStorage, Workspace } from "@rbxts/services";
 import { t } from "@rbxts/t";
 import { User } from "types";
 
 const userData = new Map<number, Map<string, string>>();
+const baseData = new Map<number, Folder>();
 
-const PlayerDataStore = DataStoreService.GetDataStore("PlayerDataStore");
+const PlayerDataStore = DataStoreService.GetDataStore("PlayerDataStore=v1.1");
+const BaseDataStore = DataStoreService.GetDataStore("BaseDataStore=v1.1");
 
 function NewData() {
 	const UserData = new Map<string, string>();
 	ReplicatedStorage.Blocks.GetChildren().forEach((child) => {
+		UserData.set(`Inventory/${child.Name}`, "0");
+	});
+	ReplicatedStorage.Tools.GetChildren().forEach((child) => {
 		UserData.set(`Inventory/${child.Name}`, "0");
 	});
 	UserData.set(`Keybinds/Inventory`, "E");
@@ -19,7 +24,44 @@ function NewData() {
 	UserData.set(`Keybinds/PickupItem`, "F");
 	UserData.set(`Keybinds/Sprint`, "LeftShift");
 	UserData.set(`Character/Health`, "100");
+	// STARTER ITEMS
+	UserData.set(`Inventory/Pickaxe`, "1");
+	UserData.set(`Inventory/Stone`, "15");
 	return UserData;
+}
+
+function NewBase(player: Player) {
+	const base = ReplicatedStorage.StarterBase.Clone();
+	base.Name = tostring(player.UserId);
+	base.Parent = Workspace;
+	baseData.set(player.UserId, base);
+}
+
+function LoadBase(player: Player) {
+	const LoadString = BaseDataStore.GetAsync(tostring(player.UserId))[0] as string;
+	if (!LoadString) return NewBase(player);
+	const base = new Instance("Folder");
+	base.Name = tostring(player.UserId);
+	base.Parent = Workspace;
+	baseData.set(player.UserId, base);
+	LoadString.split(";").forEach((block) => {
+		const block1 = block.split(":");
+		const Block = ReplicatedStorage.Blocks.FindFirstChild(block1[0])?.Clone() as Part;
+		if (!Block) return;
+		Block.Parent = base;
+		Block.Position = new Vector3(tonumber(block1[1]), tonumber(block1[2]), tonumber(block1[3]));
+	});
+}
+
+function SaveBase(player: Player) {
+	const data = baseData.get(player.UserId);
+	if (!data) return;
+	const saveString: Array<string> = [];
+	data.GetChildren().forEach((child) => {
+		if (!child.IsA("Part")) return;
+		saveString.push(`${child.Name}:${child.Position.X}:${child.Position.Y}:${child.Position.Z}`);
+	});
+	BaseDataStore.SetAsync(tostring(player.UserId), saveString.join(";"));
 }
 
 function SaveData(player: Player) {
@@ -43,12 +85,13 @@ function LoadData(player: Player) {
 }
 
 Players.PlayerAdded.Connect((player) => {
+	LoadBase(player);
 	LoadData(player);
-	print(userData.get(player.UserId));
 	ReplicatedStorage.Events.DataTunnel.FireClient(player, "SET", userData.get(player.UserId));
 });
 
 Players.PlayerRemoving.Connect((player) => {
+	SaveBase(player);
 	SaveData(player);
 });
 
@@ -65,5 +108,31 @@ ReplicatedStorage.Events.DataTunnel.OnServerEvent.Connect((player, IO, key, valu
 	}
 	if (IO === "GET") {
 		ReplicatedStorage.Events.DataTunnel.FireClient(player, "UPDATE", tostring(key), data.get(tostring(key)));
+	}
+});
+
+// ee
+ReplicatedStorage.Events.EventTunnel.OnServerEvent.Connect((player, Event, Equiped, Target, TargetSurface) => {
+	if (!Equiped) return;
+	if (Event === "ITEM-USE") {
+		return;
+	}
+	if (Event === "TOOL-ACTIVATED") {
+		const Target1 = Target as Part;
+		if (!Target1) return;
+		const data = userData.get(player.UserId) as Map<string, string>;
+		if (player.DistanceFromCharacter(Target1.Position) > 20) return;
+		if (Equiped === "Pickaxe") {
+			const HP = Target1.GetAttribute("HP") as number;
+			if (!HP) return;
+			if (HP <= 10) {
+				const item = tonumber(data.get(`Inventory/${Target1.Name}`)) as number;
+				data.set(`Inventory/${Target1.Name}`, `${item + 1}`);
+				UpdateData(player, `Inventory/${Target1.Name}`, `${item + 1}`);
+				Target1.Destroy();
+			} else {
+				Target1.SetAttribute("HP", HP - 10);
+			}
+		}
 	}
 });
